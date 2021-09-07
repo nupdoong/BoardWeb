21c230240dc98bf5b1432d657b6fedc1bd37092b
juwon_park <pjw930731@DESKTOP-3Q9RL0E>
2021-09-07 13:51:26 +0900

    class3 스프링 컨테이너 및 설정 파일 update

--- BoardWeb/src/main/java/polymorphism/TVUser.java
+++ BoardWeb/src/main/java/polymorphism/TVUser.java
@@ -1,14 +1,25 @@
 package polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 public class TVUser {
 
 	public static void main(String[] args) {
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV)factory.getBean(args[0]);
-		tv.powerOn();
-		tv.volumeUp();
-		tv.volumeDown();
-		tv.powerOff();
+		// 1. Spring 컨테이너 구동
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		
+		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lockup)
+		TV tv1 = (TV)factory.getBean("tv");
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
+		//tv.powerOn();
+		//tv.volumeUp();
+		//tv.volumeDown();
+		//tv.powerOff();
+		
+		// 3. Spring 컨테이너 종료
+		factory.close();
 	}
 
 }
