df64c788bd0c110dd23d0aad9a42a1df35958fb5
nupdoong <pjw930731@naver.com>
2021-09-09 17:27:00 +0900

    class5: 어노테이션 update

--- BoardWeb/src/main/java/polymorphism/LgTV.java
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,17 +1,42 @@
 package polymorphism;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV {
+	@Autowired
+	@Qualifier("apple")
+	private Speaker speaker;
+	public LgTV() {
+		System.out.println("===> LgTV 객체 생성");
+	}
+
+	@Override
 	public void powerOn() {
+		// TODO Auto-generated method stub
 		System.out.println("LgTV---전원 켠다.");
 	}
+
+	@Override
 	public void powerOff() {
+		// TODO Auto-generated method stub
 		System.out.println("LgTV---전원 끈다.");
 	}
+
+	@Override
 	public void volumeUp() {
-		System.out.println("LgTV---소리 올린다.");
+		// TODO Auto-generated method stub
+		//System.out.println("LgTV---소리 올린다.");
+		speaker.volumeUp();
 	}
+
+	@Override
 	public void volumeDown() {
-		System.out.println("LgTV---소리 내린다.");
+		// TODO Auto-generated method stub
+		//System.out.println("LgTV---소리 내린다.");
+		speaker.volumeDown();
 	}
 
 }
